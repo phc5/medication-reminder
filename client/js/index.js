86de3d3964f3bfd15cee102691f57908192a78fa
PaulChong <paulhyunchong@gmail.com>
2016-11-14 11:35:37 -0800

    add med-container and med-form and update package.json (include dependencies), index.html, and index.js

--- client/js/index.js
+++ client/js/index.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MedList from './components/med-list';
+import MedContainer from './components/med-container';
 // import {Provider} from 'react-redux';
 // import store from './store';
 
@@ -10,6 +10,6 @@ console.log(`Client running in ${process.env.NODE_ENV} mode`);
 let items = ["Toast", "Bread", "Eggs"];
 
 document.addEventListener('DOMContentLoaded', () => {
-	ReactDOM.render(<MedList meds={items}/>,document.getElementById('app')
+	ReactDOM.render(<MedContainer meds={items}/>,document.getElementById('app')
 	);
 });
